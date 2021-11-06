edc20b89584f5f2a8fd0a4e268fbcec9b182dbf3
GustavoKenji <gustavokomatsu4@gmail.com>
2021-11-05 22:53:46 -0300

    added index.js

---
+++ src/js/index.js
@@ -0,0 +1,4 @@
+const colourAPI = "http://colourlovers.com/api/palettes/new?format=json";
+const loginAPI = "https://reqres.in/api/login";
+
+const loginButton = document.getElementById("loginBtn");

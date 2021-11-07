56c0651bb0cbe5bb1dd20bd15a0f09f24b1aa003
GustavoKenji <gustavokomatsu4@gmail.com>
2021-11-07 18:27:28 -0300

    added js to open and close the login panel

--- src/js/index.js
+++ src/js/index.js
@@ -2,3 +2,14 @@ const colourAPI = "http://colourlovers.com/api/palettes/new?format=json";
 const loginAPI = "https://reqres.in/api/login";
 
 const loginButton = document.getElementById("loginBtn");
+const closeLoginPanelButton = document.getElementById("close-login-panel");
+const loginPanel = document.getElementById("login-panel");
+
+loginButton.addEventListener('click', () => {
+    loginPanel.setAttribute("style", "display:inherit");
+});
+
+closeLoginPanelButton.addEventListener('click', () => {
+    loginPanel.setAttribute("style", "display:none");
+});
+

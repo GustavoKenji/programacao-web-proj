2c24cec6096279a4c66038e51403aa2fa4f6c2b6
GustavoKenji <gustavokomatsu4@gmail.com>
2021-11-07 22:51:29 -0300

    implemented color search functionality

--- src/js/index.js
+++ src/js/index.js
@@ -17,6 +17,11 @@ const closeFailedLoginMessage = document.getElementById("close-failed-login-mess
 
 const colorPage = document.getElementById("color-page");
 const logoutButton = document.getElementById("logout-button");
+const inputWordColor = document.querySelector("#input-word");
+const inputWordError = document.querySelector("#input-word-error");
+const submitButtonColor = document.querySelector("#submit-word");
+const resultsPalette = document.querySelector("#results");
+let responsePalette = [];
 
 loginButton.addEventListener('click', () => {
     loginPanel.setAttribute("style", "display:inherit");
@@ -116,7 +121,7 @@ function displayColorDisplay() {
     let acces_token = recoverToken()
     if (acces_token !== "") {
         colorPage.setAttribute("style", "display:inherit");
-
+        
     } else {
         colorPage.setAttribute("style", "display:none");
     }
@@ -125,3 +130,96 @@ function displayColorDisplay() {
 logoutButton.addEventListener('click', () => {
     colorPage.setAttribute("style", "display:none");
 });
+
+inputWordColor.addEventListener('change', (event) => {
+    event.preventDefault();
+    if (inputWordColor.value.length > 3) {
+        inputWordError.innerText = "";
+    }
+});
+
+submitButtonColor.addEventListener('click', (event) => {
+    event.preventDefault();
+    if (inputWordColor.value.length > 3) {
+        let rangeValues = calculateRangeValues(transformNameInAsciiCode(inputWordColor.value));
+
+        requestColoursPallete(rangeValues).then(pallete => {
+            template(pallete);
+        });
+    } else {
+        inputWordError.innerText = "Informe mais de três caracteres"
+    }
+});
+
+function transformNameInAsciiCode(nameArray) {
+    let nameSplited = nameArray.split("");
+    return nameSplited.map(ch => {
+        return ch.charCodeAt();
+    });
+}
+
+function calculateRangeValues(listCodes) {
+    let { first, second } = separeteList(listCodes);
+    let minVal = first.reduce((accumulator, current) => accumulator + current);
+    let maxVal = second.reduce((accumulator, current) => accumulator + current);
+
+    minVal = ((359 * (maxVal - minVal) / (359 + minVal)));
+    maxVal = ((359 * (maxVal - minVal) / (359 + maxVal)));
+
+    return { min: Math.ceil(minVal), max: Math.ceil(maxVal) };
+}
+
+function separeteList(listCodes) {
+    listCodes = listCodes.sort((a, b) => a - b);
+    let middle = Math.floor(listCodes.length / 2);
+    let firstSubList = listCodes.slice(0, middle);
+    let secondSubList = listCodes.slice(middle, listCodes.length);
+
+    return { first: firstSubList, second: secondSubList };
+}
+
+function requestColoursPallete(values) {
+    let { min, max } = values;
+    let url = `${colourAPI}&hueRange=${min},${max}&jsonCallback=_callback`;
+    return $.ajax({
+        url: url,
+        crossDomain: true,
+        dataType: "jsonp",
+        jsonpCallback: '_callback',
+        complete: function (xhr, status) {
+            console.log('complete')
+        },
+        success: function (data, status, xhr) {
+            if (data.length != undefined && data.length > 0) {
+                if (data[0].colors != undefined) {
+                    responsePalette = data;
+                }
+            }
+        },
+        error: function (xhr, status, error) {
+            alert('There was a problem getting a color palette.');
+        }
+    });
+}
+
+function template(values) {
+    let randomNumber = getRandomArbitrary();
+    let palettes = values.slice(randomNumber, randomNumber + 1);
+    let palette = palettes.map((val) => {
+        return `
+            <span class="palette-title">${val.title}</span>     
+            <div class="palette-result">
+                ${val.colors.map(v => {
+                    return `<div class="palette-square" style="background:#${v};">
+                    </div>`
+                }).join("")}
+            </div>
+        `;
+    });
+
+    resultsPalette.innerHTML = palette.join("");
+}
+
+function getRandomArbitrary() {
+    return Math.random() * (20 - 0) + 0;
+  }

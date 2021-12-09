9281a9b4cf6b6f12dd54d518c197782925573444
GustavoKenji <gustavokomatsu4@gmail.com>
2021-12-08 23:52:05 -0300

    adicionando back ao projeto

---
+++ backend/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURL: 'mongodb+srv://Kenji:webproj2021@cluster0.74rxg.mongodb.net/test?authSource=admin&replicaSet=atlas-sngwiy-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true',
+    //mongoURL: 'mongodb://127.0.0.1:27017',
+    dbName: 'projeto3'
+};
---
+++ backend/controllers/LoginController.js
@@ -0,0 +1,50 @@
+const { compareSync } = require("bcryptjs");
+const User = require("../models/User");
+const Session = require("../models/Session");
+
+class LoginController {
+  constructor() {
+    this.UserModel = new User();
+    this.SessionModel = new Session();
+  }
+
+  async login(request, response) {
+    try {
+      const { body, sessionID } = request;
+      let user = await this.UserModel.findOne({ username: body.username });
+      if (user) {
+				let verifySession = await this.SessionModel.consultSession({ username: body.username });
+				if(verifySession === null || verifySession === undefined) {
+					if (compareSync(body.password, user.password)) {
+						let session = await this.SessionModel.createSession({
+							username: body.username,
+							session: sessionID,
+						});
+						return response.status(200).json({ user, sessionID });
+					} else {
+						return response.status(401).json({ error: "Não autorizado" });
+					}
+				} else {
+					return response.status(409).json({ error: "Usuário já está logado na plataforma" });
+				}
+      } else {
+        return response.status(400).json({ error: "Usuário não existe" });
+      }
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  async logout(request, response) {
+		try {
+			const { body } = request;
+			const { sessionID } = body;
+			let deletedSession = await this.SessionModel.endSession({ session: sessionID });
+			return response.status(200).json( deletedSession );
+		} catch (error) {
+			throw new Error(error);
+		}
+	}
+}
+
+module.exports = new LoginController();
